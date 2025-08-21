c998ab32d4d28a13bf30c0680b7a8a1faec2055e
plusk0 <pluskovich@outlook.com>
2025-08-21 10:39:21 +0200

    added decoding structure

--- encoder.c
+++ encoder.c
@@ -17,9 +17,7 @@
   ((byte) & 0x02 ? '1' : '0'), \
   ((byte) & 0x01 ? '1' : '0') 
 
-#define debug 0
-
-
+#define debug 1
 
 img_data_t *decodeTwoSteps(const char* filename) {
   unsigned error;
@@ -47,7 +45,7 @@ img_data_t *decodeTwoSteps(const char* filename) {
   img_data->height = height;
 
     if (debug) {
-    for (int i = 0; i < 10 && i < img_data->data_size; ++i) {
+    for (int i = 0; i < 3 && i < img_data->data_size; ++i) {
         unsigned char *pixel = (unsigned char *)&img_data->data[i * 4];
         printf("Pixel %d: R=%u G=%u B=%u A=%u\n",
             i,
@@ -64,8 +62,6 @@ img_data_t *decodeTwoSteps(const char* filename) {
   // Note: Caller is responsible for freeing the image memory
 }
 
-
-
 int pixel_changer(char *pixel, int hex) {
   if (pixel == NULL) {
     printf("Error: pixel pointer is NULL\n");
@@ -80,7 +76,6 @@ int pixel_changer(char *pixel, int hex) {
       break;
 
     case 1: 
-
       if (debug) {printf("Changing pixel %u\n", *pixel);}
       *pixel -= 1; 
       if (debug) {printf("Changed pixel %u\n", *pixel);} 
@@ -107,34 +102,40 @@ int pixel_changer(char *pixel, int hex) {
 
 txt_data_t *get_text_data(const char *filename, int img_size) {
   FILE* fp = 0;
+
+    if ( filename == NULL ) {
+    printf("Error- No filename provided for text data\n");
+    return NULL;
+  }
+
+  if( ( fp = fopen( filename, "r" ) ) == NULL ) {
+		printf("Error- Unable to open %s\n", filename );
+		exit( -1 );
+	} 
+
   txt_data_t *text_data = (txt_data_t *)malloc(sizeof(txt_data_t));
     if (text_data == NULL) {
         printf("Error allocating memory for text_data\n");
         return NULL;
     }
 
-  if ( filename == NULL ) {
-    printf("Error- No filename provided for text data\n");
+    // -> malloc for case whole img is used
+  char *text = (char *)malloc(img_size  + 1);
+  if (text == NULL) {
     free(text_data);
-    return NULL;
+    printf("Error- Unable to allocate text data memory\n");
+    exit ( -1 );
   }
 
-  if( ( fp = fopen( filename, "r" ) ) == NULL ) {
-		printf("Error- Unable to open %s\n", filename );
-        free(text_data);
-		exit( -1 );
-	} 
-
-  text_data->data = (char *)malloc(strlen(text_data->data) + 1);
+  text_data->data = text;
   fgets(text_data->data, img_size, fp);
-
   fclose(fp);
 
   text_data->data_size = strlen(text_data->data);
   if (debug) {printf("Decoded text: %s\n", text_data->data);}
 
   return text_data;
-    // Note: Caller is responsible for freeing the text_data memory
+    // Caller is responsible for freeing the text_data (and ->data) memory
 }
 
 int hide_text_in_image(img_data_t *img_data, txt_data_t *text_data) {
@@ -161,6 +162,7 @@ int hide_text_in_image(img_data_t *img_data, txt_data_t *text_data) {
     unsigned char part3 = (byte >> 2) & 0x03; // bits 3-2
     unsigned char part4 = byte & 0x03; 
 
+
     if (debug) {printf("Parts: %u %u %u %u\n", part1, part2, part3, part4);}
 
     pixel_changer(&image[i * 4 + 0], part1);
@@ -170,7 +172,7 @@ int hide_text_in_image(img_data_t *img_data, txt_data_t *text_data) {
 
     if (debug) {
       printf("Stored image data after hiding text:\n");
-      for (int j = 0; j < 10 && j < img_size; ++j) {
+      for (int j = 0; j < 3 && j < img_size; ++j) {
         printf("Pixel %d: R=%u G=%u B=%u A=%u\n",
             j,
             image[j * 4 + 0],
@@ -180,8 +182,6 @@ int hide_text_in_image(img_data_t *img_data, txt_data_t *text_data) {
       }
     }
   }
-
-
   return 0;
 }
 
--- main.c
+++ main.c
@@ -9,29 +9,54 @@
 
 int main(int argc, char *argv[]) {
 
-  bool done = false;
-  const char* image_filename = argc > 1 ? argv[1] : "test_files/test.png";
-  const char* text_filename = argc > 2 ? argv[2] : "test_files/test.txt";
 
-  img_data_t *initial_image_data = decodeTwoSteps(image_filename);
-  txt_data_t *text_data = get_text_data(text_filename, initial_image_data->data_size);
+  //syntax e: ./stego e file_to_encode.png text_to_encode.txt
+  //syntax d: ./stego d file_to_decode.png unencoded_reference_png
 
-    if (initial_image_data == NULL || text_data == NULL || done == true) {
+  const char* operation = argc > 1 ? argv[1] : "e";
+
+  if (!strcmp(operation, "e")) {
+
+    const char* image_filename = argc > 2 ? argv[2] : "test_files/test.png";
+    const char* text_filename = argc > 3 ? argv[3] : "test_files/test.txt";
+    
+    img_data_t *initial_image_data = decodeTwoSteps(image_filename);
+    txt_data_t *text_data = get_text_data(text_filename, initial_image_data->data_size);
+
+    if (initial_image_data == NULL || text_data == NULL) {
         printf("Error processing image or text data.\n");
         free(initial_image_data);
         free(text_data->data);
         free(text_data);
-        return 0;
+        return 1;
     }
 
     hide_text_in_image(initial_image_data, text_data);
 
-    encodeTwoSteps("encoded_image.png", (unsigned char *)initial_image_data->data, initial_image_data->width, initial_image_data->height);
+    encodeTwoSteps("test_files/encoded_image.png", (unsigned char *)initial_image_data->data, initial_image_data->width, initial_image_data->height);
     
     free(initial_image_data);
     free(text_data->data);
     free(text_data);
+  return 0;
+  } else if (!strcmp(operation, "d")) {
 
+    const char* encoded_image_filename = argc > 2 ? argv[2] : "test_files/encoded_image.png";
+    const char* ref_image_filename = argc > 3 ? argv[3] : "test_files/test.png";
 
+    //encoder.c houses the image decoder, is reused for decoding
+    img_data_t *encoded_image_data = decodeTwoSteps(encoded_image_filename); 
+    img_data_t *reference_image_data = decodeTwoSteps(ref_image_filename); 
+    //txt_data_t *text_data = decode_image(initial_image_data, encoded_image_data);
+
+    int diff = *(int *)encoded_image_data->data ^ *(int *)reference_image_data->data;
+
+    printf("Difference detected (yes if not 0): %d text decoding not yet implemented", diff);
+
+  } else {
+    printf("Invalid operation type: '%s' -> use 'e' for encoding and 'd' for decoding", operation);
+    return -1;
+  }
+  printf("\nDone\n");
   return 0;
 }
